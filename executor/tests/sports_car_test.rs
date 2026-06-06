168dd937d147252456910d2e5b14e6e2ae8d04eb
Zephyr <180604036@qq.com>
2026-06-06 14:54:06 +0800

    add test cases of command L for sports car

--- executor/tests/sports_car_test.rs
+++ executor/tests/sports_car_test.rs
@@ -40,6 +40,46 @@ mod move_tests{
     }
 }
 
+mod turn_left_tests{
+    use super::*;
+    #[test]
+    fn should_return_y_plus_1_and_facing_n_given_command_is_l_and_facing_is_e(){
+        let original_pose = Pose::new(0,0,'E');
+        let mut executor = Executor::with_pose(original_pose);
+        executor.execute("L");
+        let expected_pose = Pose::new(0,1,'N');
+        assert_eq!(expected_pose,executor.query());
+    }
+
+    #[test]
+    fn should_return_y_plus_1_and_facing_s_given_command_is_bl_and_facing_is_e(){
+        let original_pose = Pose::new(0,0,'E');
+        let mut executor = Executor::with_pose(original_pose);
+        executor.execute("BL");
+        let expected_pose = Pose::new(0,1,'S');
+        assert_eq!(expected_pose,executor.query());
+    }
+
+    #[test]
+    fn should_return_x_plus_1_y_plus_1_and_facing_n_given_command_is_fl_and_facing_is_e(){
+        let original_pose = Pose::new(0,0,'E');
+        let mut executor = Executor::with_pose(original_pose);
+        executor.execute("FL");
+        let expected_pose = Pose::new(1,1,'N');
+        assert_eq!(expected_pose,executor.query());
+    }
+
+    #[test]
+    fn should_return_x_mius_1_y_plus_1_and_facing_s_given_command_is_fbl_and_facing_is_e(){
+        let original_pose = Pose::new(0,0,'E');
+        let mut executor = Executor::with_pose(original_pose);
+        executor.execute("FBL");
+        let expected_pose = Pose::new(-1,1,'S');
+        assert_eq!(expected_pose,executor.query());
+    }
+
+
+}
 
 
 
